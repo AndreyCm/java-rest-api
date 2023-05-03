b09a84f3e5a5364a7ebf511955574d70f87e08c9
Andrey Chemyrtan <andrey.chemyrtan@gmail.com>
2023-05-03 18:57:17 +0300

    Init commit

---
+++ src/main/java/com/example/javacontrol/JavaControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javacontrol/JavaControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
